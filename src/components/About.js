ea29eb86c5b020aa62d7a5a65622cf0b64b309a5
Hosea <karanja.mungai23@gmail.com>
2024-10-29 14:25:00 +0300

    modifies react components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
-
-function About() {
+import user from "../data/user";
+import Links from "./Links";
+function About(props) {
+  const github  = user.links.github 
+  const linkedin = user.links.linkedin
+  
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>{props.about}</h2>
+      {props.bio && props.bio !== "" ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github = {github} linkedin={linkedin}/> 
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,19 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+//console.log(user);
+
 
 function App() {
+  const { name, color,city,bio,about } = user
+  const github  = user.links.github 
+  console.log(color + city + name + github)
   return (
+    
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} color={color} city={city}/>
+      <About bio={bio} about ={about}/>
     </div>
   );
 }
